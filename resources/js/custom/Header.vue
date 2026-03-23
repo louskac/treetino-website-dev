4ebb710ecdaa1383b802b93cb04baf6d2a9a6bb9
Adam Baranowski <adam.beranovsky@gmail.com>
2026-03-24 00:47:27 +0100

    Edited Header.vue

--- resources/js/custom/Header.vue
+++ resources/js/custom/Header.vue
@@ -3,18 +3,36 @@
         <div
             class="mx-auto flex w-full max-w-[1400px] justify-between p-6 sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)]"
         >
-            <div class="logo flex gap-3 text-white">
-                <LogoType class="fill-current text-white dark:text-white" />
-            </div>
+            <div
+                class="flex w-full justify-between rounded-2xl bg-black/20 p-6 backdrop-blur-2xl border border-white/20"
+            >
+                <div class="logo my-auto flex gap-3 text-white">
+                    <LogoType class="fill-current text-white dark:text-white" />
+                </div>
 
-            <div class="dark-switch my-auto text-white">
-                <button
-                    @click="toggleDark"
-                    class="cursor-pointer p-1 transition-opacity hover:opacity-70"
-                >
-                    <HalfMoon v-if="isDark" class="h-5 w-5 text-white" />
-                    <SunLight v-else class="h-5 w-5 text-white" />
-                </button>
+                <div class="flex gap-4">
+                    <div class="dark-switch my-auto text-white">
+                        <button
+                            @click="toggleDark"
+                            class="cursor-pointer p-1 transition-opacity hover:opacity-70"
+                        >
+                            <HalfMoon
+                                v-if="isDark"
+                                class="h-4.5 w-4.5 text-white"
+                                stroke-width="2"
+                            />
+                            <SunLight
+                                v-else
+                                class="h-4.5 w-4.5 text-white"
+                                stroke-width="2"
+                            />
+                        </button>
+                    </div>
+
+                    <div class="my-auto text-white">
+                        <ButtonWhite>Preorder Now</ButtonWhite>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
@@ -24,6 +42,9 @@
 import { SunLight, HalfMoon } from '@iconoir/vue';
 import { ref, onMounted } from 'vue';
 import LogoType from '@/custom/LogoType.vue';
+import ButtonPrimary from '@/custom/ButtonPrimary.vue';
+import ButtonSecondary from '@/custom/ButtonSecondary.vue';
+import ButtonWhite from '@/custom/home/ButtonWhite.vue';
 
 // 1. Track if we are currently dark
 const isDark = ref(false);
--- resources/js/custom/LogoType.vue
+++ resources/js/custom/LogoType.vue
@@ -1,6 +1,6 @@
 <template>
     <svg
-        class="h-7"
+        class="h-6"
         id="Layer_1"
         data-name="Layer 1"
         xmlns="http://www.w3.org/2000/svg"
