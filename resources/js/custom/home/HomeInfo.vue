60aa188db813018d6cb52473a313e13f03adf2e8
Adam Baranowski <adam.beranovsky@gmail.com>
2026-03-24 11:58:23 +0100

    Fixed Padding in HomeInfo

--- resources/js/custom/home/HomeInfo.vue
+++ resources/js/custom/home/HomeInfo.vue
@@ -5,7 +5,7 @@
         ></div>
 
         <div
-            class="relative mx-auto max-w-[1200px] px-6 py-20 sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)]"
+            class="relative mx-auto max-w-[1200px] py-20 sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)]"
         >
             <div class="grid grid-cols-3 gap-6">
                 <div
