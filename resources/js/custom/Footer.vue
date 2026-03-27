b422076c513261bb35df2368e575e85fb1d28513
Adam Baranowski <adam.beranovsky@gmail.com>
2026-03-27 12:59:16 +0100

    Footer Format corrected

--- resources/js/custom/Footer.vue
+++ resources/js/custom/Footer.vue
@@ -1,7 +1,11 @@
 <template>
-    <footer>
+    <footer class="relative">
         <div
-            class="mx-auto w-full max-w-[1200px] justify-between px-6 sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)]"
+            class="absolute left-1/2 h-full max-w-[1400px] -translate-x-1/2 border-r border-l border-r-black/20 border-l-black/20 [mask-image:linear-gradient(to_bottom,black_0%,transparent_100%)] sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)] dark:border-r-white/20 dark:border-l-white/20"
+        ></div>
+
+        <div
+            class="mx-auto w-full max-w-[1400px] justify-between px-6 sm:w-[500px] md:w-[700px] lg:w-[calc(100%-200px)] xl:w-[calc(100%-400px)]"
         >
             <div class="flex flex-col">
                 <div class="w-full py-6">
@@ -12,10 +16,10 @@
                             />
                         </div>
 
-                        <div class="socials flex gap-5 my-auto">
-                            <Instagram class="h-5 w-5"/>
-                            <Linkedin class="h-5 w-5"/>
-                            <X class="h-5 w-5"/>
+                        <div class="socials my-auto flex gap-5">
+                            <Instagram class="h-5 w-5" />
+                            <Linkedin class="h-5 w-5" />
+                            <X class="h-5 w-5" />
                         </div>
                     </div>
                 </div>
@@ -25,10 +29,6 @@
 </template>
 
 <script setup lang="ts">
-import {
-    Instagram,
-    Linkedin,
-    X,
-} from '@iconoir/vue';
+import { Instagram, Linkedin, X } from '@iconoir/vue';
 import LogoType from '@/custom/LogoType.vue';
 </script>
