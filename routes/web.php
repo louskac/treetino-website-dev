d7e501b469b19cb875e691d3afd6bd63e2d240c9
JestrabikR <jestrab2003@gmail.com>
2026-03-31 12:21:56 +0200

    Added empty configurator page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\ConfiguratorController;
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/configurator', [ConfiguratorController::class, 'index'])->name('configurator');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
