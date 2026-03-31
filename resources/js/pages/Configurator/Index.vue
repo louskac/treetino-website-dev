5d052d564638468a013a4b04a29c5b03a55f498a
JestrabikR <jestrab2003@gmail.com>
2026-03-31 20:47:05 +0200

    Added Battery Step to configurator

--- resources/js/pages/Configurator/Index.vue
+++ resources/js/pages/Configurator/Index.vue
@@ -29,6 +29,9 @@
                 <div class="border-t border-t-blue/10 dark:border-white/10 pt-7">
                     <ConfiguratorConnectivityStep v-model="selectedConnectivity" />
                 </div>
+                <div class="border-t border-t-blue/10 dark:border-white/10 pt-7">
+                    <ConfiguratorBatteryStep v-model="selectedBattery" />
+                </div>
             </div>
         </div>
 
@@ -43,6 +46,7 @@ import ConfiguratorProductHeader from '@/custom/configurator/ConfiguratorProduct
 import ConfiguratorColorStep from '@/custom/configurator/ConfiguratorColorStep.vue';
 import ConfiguratorLeafColorStep from '@/custom/configurator/ConfiguratorLeafColorStep.vue';
 import ConfiguratorConnectivityStep from '@/custom/configurator/ConfiguratorConnectivityStep.vue';
+import ConfiguratorBatteryStep from '@/custom/configurator/ConfiguratorBatteryStep.vue';
 
 const products = [
     { id: 'strom-v1', label: 'Strom V1', params: { power: '800 W',  dailyProduction: '3,2 kWh', roi: '7 let' } },
@@ -54,6 +58,7 @@ const selectedProductId = ref('strom-v1');
 const selectedColorId = ref('white');
 const selectedLeafColorId = ref('green');
 const selectedConnectivity = ref('none');
+const selectedBattery = ref('none');
 
 const selectedProduct = computed(() => products.find(p => p.id === selectedProductId.value)!);
 
